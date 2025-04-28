93bd879b30b4e3d95c3d9d9210851cd4159cc335
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-04-28 14:46:35 +0200

    cleaned up the code inserted the clouds

---
+++ classes.js/cloud.classes.js
@@ -0,0 +1,9 @@
+class Cloud extends movableObject{
+
+    x = 0 + Math.random() * 500
+    y = 25
+    width = 400
+    constructor(){
+        super().loadImage('img/img_pollo_locco/img/5_background/layers/4_clouds/1.png')
+    }
+}
--- classes.js/world.class.js
+++ classes.js/world.class.js
@@ -4,14 +4,19 @@ class World{
         new Chicken(),
         new Chicken(),
         new Chicken(),
+    ];
+
+    clouds =[
+        new Cloud(),
+    ];
+    backgroundObjects =[
+        
     ]
     ctx;
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas
         this.draw()
-    
-        
     }
 
 
@@ -20,14 +25,18 @@ class World{
     draw(){
         this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
         this.enemies.forEach(chicken => {
-            this.ctx.drawImage(chicken.img,chicken.x,chicken.y,chicken.width,chicken.height)    
+            this.addToMap(chicken)
         });
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
+        this.clouds.forEach(cloud => {
+            this.addToMap(cloud)
+        });
+        this.addToMap(this.character)
+        requestAnimationFrame(() => {
+            this.draw();
+        });
+    }
 
-
-        let self = this
-        requestAnimationFrame(function() {
-            self.draw();
-        })
+    addToMap(mo){
+        this.ctx.drawImage(mo.img,mo.x,mo.y,mo.width,mo.height)    
     }
 }
