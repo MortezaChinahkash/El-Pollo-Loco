833fee7f963f6fbf0494bfa5faffc220a823c903
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-04-28 10:50:32 +0200

    first attempts without OOP

--- js/game.js
+++ js/game.js
@@ -1,7 +1,11 @@
 let canvas;
+let ctx;
 let character = new Image();
 
 function init(){
     canvas = document.getElementById("canvas");
-    character.src = 
+    ctx = canvas.getContext('2d');
+    character.src = "../img/img_pollo_locco/img/2_character_pepe/2_walk/W-21.png"
+
+    ctx.drawImage(character, 20, 20, 50, 150)
 }
